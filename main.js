557782dd0b1406a2b3c520247e164368d202dad4
Niçoise Nkounkou <nicoise.nkounkou@edu.devinci.fr>
2022-12-26 18:14:43 +0100

    second part JS

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ function handleSubmit(e){
  // console.log(results)
 
     showResults(results)
+    addColors(results)
 }
 
 const titleResult = document.querySelector(".results h2");
@@ -85,10 +86,32 @@ switch(errorsNumber){
     
 }
 
-
 }
 
+const questions = document.querySelectorAll(".question-block");
 
+function addColors(results) {
+    results.forEach((response, index) => {
+        if(results[index]){
+            questions[index].style.backgroundImage = 
+            "linear-gradient(to right, #a8ff78, #78ffd6)"
+        } else {
+            questions[index].style.backgroundImage =
+             "linear-gradient(to right, #f5567b, #fd674c)"
+        }
+    })
+}
 
+const radioInputs = document.querySelectorAll("input[type='radio']")
 
+radioInputs.forEach(radioInputs => radioInputs.addEventListener('input', resetColor ))
+
+function resetColor(e){
+
+   const index = e.target.getAttribute("name").slice(1) -1;
+   const parentQuestionBlock = questions[index];
+
+   parentQuestionBlock.style.backgroundColor = "#f1f1f1";
+   parentQuestionBlock.style.backgroundImage = "none";
+}
 
